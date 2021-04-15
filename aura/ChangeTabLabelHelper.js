052d5638eca38ed333ce8d4f5582de435e43c71c
sf-kiwi-cpat <61746287+sf-kiwi-cpat@users.noreply.github.com>
2021-04-15 16:05:44 -0400

    Changes to remove defined objects for component

--- aura/ChangeTabLabelHelper.js
+++ aura/ChangeTabLabelHelper.js
@@ -3,28 +3,29 @@
     // We
     init : function(component, event) {
         //console.log("init Called. component.get('v.simpleRecord'):" + component.get('v.simpleRecord'));
-        var foundFieldValue = false;
+        let foundFieldValue = false;
+        let value = "";
         if('v.objectField'!=null) {
             //get the field name
-            var field = component.get('v.objectField');
+            let field = component.get('v.objectField');
             //set key for values lookup
-            var key = 'v.simpleRecord.'+field;
+            let key = 'v.simpleRecord.'+field;
             //use the key to lookup value returned in LDS
-            var value = component.get(key);
+            value = component.get(key);
             //console.log("key=" + key + " - value=" + value);
-            if (value != null) 
+            if (value) 
             {
                 foundFieldValue = true;
-                // set the value to and attribute to be used in your component=
+                // set the value to be used later 
                 component.set('v.passThroughValue', value);
             }
         }
         
-        var useContact = component.get('v.useContact');
+        let useContact = component.get('v.useContact');
         //console.log("useContact=" + useContact + " - foundFieldValue=" +foundFieldValue);
         if (!useContact && foundFieldValue)
         {
-			//console.log("Setting label as object field");
+			//console.log("Setting label as object field value");
             component.set('v.tabLabel', value);
             this.setTabLabel(component);
        	}
@@ -37,16 +38,16 @@
     ,
     createGetNewLabelAction : function(component)
     {
-        var action = component.get("c.getNewLabel");
+        let action = component.get("c.getNewLabel");
         action.setParams({ recordId: component.get("v.recordId")});
         action.setCallback(this,
                $A.getCallback(function(response1)
                {
-                   var state = response1.getState();             
+                   let state = response1.getState();             
                    if (state === "SUCCESS")  
                    {
-                       var useContact = component.get('v.useContact');
-                       var value = response1.getReturnValue(); // This gets the answer from the Controller
+                       let useContact = component.get('v.useContact');
+                       let value = response1.getReturnValue(); // This gets the answer from the Controller
                        if (value != null)
                        {
                            component.set('v.tabLabel', value);
@@ -66,12 +67,12 @@
     },
     setTabLabel : function(component)
     {
-        var label = component.get('v.tabLabel');
+        let label = component.get('v.tabLabel');
         if (label != null)
         {
-            var workspaceAPI = component.find("workspace");
+            let workspaceAPI = component.find("workspace");
             workspaceAPI.getFocusedTabInfo().then(function(response) {
-                var focusedTabId = response.tabId;
+                let focusedTabId = response.tabId;
                 // This sets the actual tab label with the result
                 workspaceAPI.setTabLabel({
                     tabId: focusedTabId,
